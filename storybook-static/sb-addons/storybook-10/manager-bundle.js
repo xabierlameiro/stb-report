e3a47f735df053eca6ff0c73e86006ad123cc2bd
GitHub Action <action@github.com>
2024-03-16 19:38:27 +0000

    Update stb-report

---
+++ storybook-static/sb-addons/storybook-10/manager-bundle.js
@@ -0,0 +1,3 @@
+try{
+(()=>{var c=__STORYBOOK_ADDONS__,{addons:o,types:d,mockChannel:h}=__STORYBOOK_ADDONS__;var f=__STORYBOOK_THEMING__,{CacheProvider:u,ClassNames:x,Global:S,ThemeProvider:y,background:R,color:C,convert:N,create:a,createCache:k,createGlobal:v,createReset:B,css:D,darken:G,ensure:K,ignoreSsrWarning:P,isPropValid:Y,jsx:I,keyframes:A,lighten:E,styled:H,themes:M,typography:j,useTheme:w,withTheme:U}=__STORYBOOK_THEMING__;var s=a({base:"light",brandTitle:"Return to home page",brandUrl:"https://xabierlameiro.com",brandImage:"https://xabierlameiro.com/favicon.png",brandTarget:"_self"});o.setConfig({theme:s,panelPosition:"right"});})();
+}catch(e){ console.error("[Storybook] One of your manager-entries failed: " + import.meta.url, e); }
